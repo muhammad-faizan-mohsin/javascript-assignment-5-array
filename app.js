8a23c1f47a3019a9accf4c5e19fe7e763c4fb907
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-02-11 19:45:14 +0500

    8 questions compeleted

---
+++ app.js
@@ -0,0 +1,263 @@
+// Exersice 1
+// Answer 1
+
+// var city = prompt("Enter your city name");
+
+// if(city === "karachi"){
+//    document.write("Welcome to city of lights")
+// }
+
+
+//Answer 2
+
+// var gender = prompt("Enter your gander");
+
+// if(gender === "male"){
+//     document.write("Good Morning Sir.")}
+// if(gender === "female"){
+//     document.write("Good Morning Ma’am..")
+// }
+
+
+// Answer 3
+
+// var gender = prompt("Enter color");
+
+// if(gender == "Red"){
+//     document.write("Must Stop")}
+// if(gender == "Yellow"){
+//     document.write("Ready to move")
+// }
+// if(gender == "Green"){
+//     document.write("Move now")
+// }
+
+
+
+// Answer 4
+// var fuel = prompt("Enter your fuel in leter ");
+// if (fuel < "0.25"){
+//     document.write("Please refill the fuel in your car")
+// }
+
+
+// Answer 5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+// answer 6 
+
+// var marksObt = prompt("Enter Your Marks");
+// var totalMarks = 300;
+// var percentage = marksObt / totalMarks * 100
+// if (percentage < 60){
+//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : Fail" +"<br />" + "Remarks: Sorry")
+// }
+
+// else if (percentage >= 60 && percentage <= 70){
+//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : b" +"<br />" + "Remarks: You need to improve")
+// }
+
+
+// else if (percentage >= 70 && percentage <= 80){
+//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : a" +"<br />" + "Remarks: Good")
+// }
+
+
+// else if (percentage >= 80 && percentage <= 100){
+//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : A-one grade" +"<br />" + "Remarks: Excellent")
+// }
+
+
+
+// answer 7
+// var num = 2;
+// var guessNum = +prompt("Enter Any Number (ranging from 1 to 10)");
+
+// if(num === guessNum){
+// alert("Bingo! Correct answer.")
+// }
+
+
+// else if( ++num == guessNum  ){
+//     alert("Close enough to the correct answer.")
+//     }
+//     else{
+//     alert   ("not correct")
+//     }
+
+// answer 8
+
+// var check = prompt("enter number")
+// var divide = check % 3;
+
+// if(divide == 0){
+//     alert("This number is divisible by 3")
+// }
+
+// answer 9
+// var num = +prompt("enter num")
+// var even = num % 2;
+
+// if(even === 0){
+//     alert("its even")
+// }
+// else if(even !== 0){
+//     alert("its odd")
+// }
+
+// answer 10
+
+// var temp = +prompt("Enter Temperature")
+// if(temp > 10 && temp < 20 ){
+// document.write("OMG! Today’s weather is so Cool.")
+// }
+// else if(temp > 20 && temp < 30){
+//     document.write("Today’s Weather is cool.")
+//     }
+//     else if(temp > 30 && temp < 40){
+//         document.write("The Weather today is Normal.")
+//         }
+//         else if(temp > 40 && temp < 50){
+//             document.write("It is too hot outside.")
+//             }
+
+// answer 11
+
+// var a = +prompt("enter First value");
+// var operator = prompt("enter Operation");
+// var b = +prompt("enter Second value");
+// if(operator === "+"){
+//     alert(a+b)
+// }
+// else if(operator === "-"){
+//     alert(a-b)
+// }
+// else if(operator === "*"){
+//     alert(a*b)
+// }
+// else if(operator === "/"){
+//     alert(a/b)
+// }
+// else if(operator === "%"){
+//     alert(a%b)
+// }
+
+
+
+// Exersice
+// ans no 1
+
+
+// ans no 2
+// var num1 = prompt("enter first value");
+// var num2 = prompt("enter second value");
+
+// if(num1 < num2){
+//     alert("this is larger")
+// }
+// if(num1 === num2){
+//     alert("this is Equal")
+// }
+
+// ans no 3
+// var num = prompt("enter value");
+// if(num > 0){
+//     alert("this is positive")
+// }else if(num < 0){
+//     alert("this is negative")
+// }else if(num == 0){
+//     alert("this is equal")
+// }
+
+// ans no 4
+// var vowels = prompt("enter vowels")
+
+// if(vowels == "a" || vowels =="A"){
+//     alert("Your answer is correct")
+// }else if(vowels == "e" || vowels =="E"){
+//     alert("Your answer is correct")
+// }else if(vowels == "i" || vowels =="I"){
+//     alert("Your answer is correct")
+// }else if(vowels == "o" || vowels =="O"){
+//     alert("Your answer is correct")
+// }else if(vowels == "u" || vowels =="U"){
+//     alert("Your answer is correct")
+// }else {
+//  alert("not correct")
+// }
+
+// ans no 5
+// var pass = "helloworld"
+
+// var val = prompt("enter password");
+
+// if(val == ""){
+// alert("Please enter your password")
+// }else if(pass == val){
+//     alert("Correct! The password you entered matches the original password.")
+// }else{
+//     alert("incorrect")
+// }
+
+// ans no 6
+// var greeting;
+// var hour = 13;
+// if (hour > 18) {
+// greeting = "Good day";
+// console.log(greeting)
+// }
+// else{
+// greeting = "Good evening";
+// console.log(greeting)
+// }
+
+// ans no 7
+var time = +prompt("enter time in 24 hours format")
+
+if(time > 12){
+    alert(cls)
+}else{
+    alert(time)
+}
+
+
