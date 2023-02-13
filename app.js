cd2d18f9324c2b5cb7accd56d0706aa979f48929
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-02-14 03:40:21 +0500

    half compeleted

--- app.js
+++ app.js
@@ -1,263 +1,117 @@
-// Exersice 1
-// Answer 1
+// array
+// Answer no 1
+// var a = ["faizan","hasnain"];
 
-// var city = prompt("Enter your city name");
+// Answer no 2
+// var a = new Array("faizan","hussain")
+// console.log(a)
 
-// if(city === "karachi"){
-//    document.write("Welcome to city of lights")
-// }
+//Answer no 3 
+// var a = ["faizan", "hassan","kashan"];
+// console.log(a)
 
+// Answer no 4
+// var a = [0, 1,2];
+// console.log(a)
 
-//Answer 2
+// Answer no 5
+// var a = [true, false ,true];
+// console.log(a)
 
-// var gender = prompt("Enter your gander");
+// Answer no 6
+// var a = ["faizan", 4 ,true];
+// console.log(a)
 
-// if(gender === "male"){
-//     document.write("Good Morning Sir.")}
-// if(gender === "female"){
-//     document.write("Good Morning Ma’am..")
-// }
+// Answer no 7
+// var edu = ["SSC","HSC","BCS","BS","BCOM","MS","M. Phil","PhD"]
 
+// document.write("<h1>Qualification</h1>"+
+//     "<ol><li>"+edu[1]+"</li> "+
+//     "<li>"+edu[2]+"</li>"+
+//     "<li>"+edu[3]+"</li>"+
+//     "<li>"+edu[4]+"</li>"+
+//     "<li>"+edu[5]+"</li>"+
+//     "<li>"+edu[6]+"</li>"+
+//     "<li>"+edu[7]+"</li>"+
+//     "<li>"+edu[8]+"</li>"+
+//     "<li>"+edu[9]+"</li> </ol>"
 
-// Answer 3
+// )
 
-// var gender = prompt("Enter color");
+// Answer no 8
+// var stu = ['faizan','hussain','hasnain'];
+// var marks = [320,230,480]
+// var tmarks = 500;
+// var per = marks[0] / tmarks * 100;
+// var per2 = marks[1] / tmarks * 100;
+// var per3 = marks[2] / tmarks * 100;
 
-// if(gender == "Red"){
-//     document.write("Must Stop")}
-// if(gender == "Yellow"){
-//     document.write("Ready to move")
-// }
-// if(gender == "Green"){
-//     document.write("Move now")
-// }
-
-
-
-// Answer 4
-// var fuel = prompt("Enter your fuel in leter ");
-// if (fuel < "0.25"){
-//     document.write("Please refill the fuel in your car")
-// }
-
-
-// Answer 5
-// var a = 4;
-// if (++a === 5){
-// alert("given condition for variable a is true");
-// }
-
-// var b = 82;
-// if (b++ === 83){
-// alert("given condition for variable b is true");
-// }
-// var c = 12;
-// if (c++ === 13){
-// alert("condition 1 is true");
-// }
-// if (c === 13){
-// alert("condition 2 is true");
-// }
-// if (++c < 14){
-// alert("condition 3 is true");
-// }
-// if(c === 14){
-// alert("condition 4 is true");
-// }
-
-// var materialCost = 20000;
-// var laborCost = 2000;
-// var totalCost = materialCost + laborCost;
-// if (totalCost === laborCost + materialCost){
-// alert("The cost equals");
-// }
-//  if (true){
-// alert("True");
-// }
-// if (false){
-// alert("False");
-// }
-
-
-// if("car" < "cat"){
-// alert("car is smaller than cat");
-// }
-
-
-// answer 6 
-
-// var marksObt = prompt("Enter Your Marks");
-// var totalMarks = 300;
-// var percentage = marksObt / totalMarks * 100
-// if (percentage < 60){
-//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : Fail" +"<br />" + "Remarks: Sorry")
-// }
-
-// else if (percentage >= 60 && percentage <= 70){
-//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : b" +"<br />" + "Remarks: You need to improve")
-// }
-
-
-// else if (percentage >= 70 && percentage <= 80){
-//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : a" +"<br />" + "Remarks: Good")
-// }
-
-
-// else if (percentage >= 80 && percentage <= 100){
-//     document.write("<h1>Mark Sheet</h1>" +"<br />" + "Total Marks: " + totalMarks +"<br />" + "Marks Obtained: " + marksObt +"<br />" + "percentage: " +percentage+"<br />" +"grade : A-one grade" +"<br />" + "Remarks: Excellent")
-// }
+// document.write("score of "+ stu[0] +" is "+marks[0] +" precentage "+ per +"%"+"<br/>")
+// document.write("score of "+ stu[1] +" is "+marks[1] +" precentage "+ per2+"%"+"<br/>")
+// document.write("score of "+ stu[2] +" is "+marks[2] +" precentage "+ per3+"%"+"<br/>")
 
 
 
-// answer 7
-// var num = 2;
-// var guessNum = +prompt("Enter Any Number (ranging from 1 to 10)");
-
-// if(num === guessNum){
-// alert("Bingo! Correct answer.")
-// }
+// Answer no 9
+// var clr = ["red", "blue", "green"]
 
 
-// else if( ++num == guessNum  ){
-//     alert("Close enough to the correct answer.")
-//     }
-//     else{
-//     alert   ("not correct")
-//     }
+// clr.unshift(prompt("enter"))
+// clr.push(prompt("enter"))
+// console.log(clr)
 
-// answer 8
+// clr.push("purple")
 
-// var check = prompt("enter number")
-// var divide = check % 3;
+// console.log(clr)
 
-// if(divide == 0){
-//     alert("This number is divisible by 3")
-// }
+// clr.unshift("orange","pink")
 
-// answer 9
-// var num = +prompt("enter num")
-// var even = num % 2;
+// console.log(clr)
 
-// if(even === 0){
-//     alert("its even")
-// }
-// else if(even !== 0){
-//     alert("its odd")
-// }
+// clr.shift("orange","pink")
 
-// answer 10
+// console.log(clr)
 
-// var temp = +prompt("Enter Temperature")
-// if(temp > 10 && temp < 20 ){
-// document.write("OMG! Today’s weather is so Cool.")
-// }
-// else if(temp > 20 && temp < 30){
-//     document.write("Today’s Weather is cool.")
-//     }
-//     else if(temp > 30 && temp < 40){
-//         document.write("The Weather today is Normal.")
-//         }
-//         else if(temp > 40 && temp < 50){
-//             document.write("It is too hot outside.")
-//             }
+// clr.pop("orange","pink")
 
-// answer 11
+// console.log(clr)
 
-// var a = +prompt("enter First value");
-// var operator = prompt("enter Operation");
-// var b = +prompt("enter Second value");
-// if(operator === "+"){
-//     alert(a+b)
-// }
-// else if(operator === "-"){
-//     alert(a-b)
-// }
-// else if(operator === "*"){
-//     alert(a*b)
-// }
-// else if(operator === "/"){
-//     alert(a/b)
-// }
-// else if(operator === "%"){
-//     alert(a%b)
-// }
+// clr.splice(2,1,"lemon")
+
+// console.log(clr)
 
 
+// clr.splice(2,1)
 
-// Exersice
-// ans no 1
+// console.log(clr)
 
+// Answer no 10
 
-// ans no 2
-// var num1 = prompt("enter first value");
-// var num2 = prompt("enter second value");
+// var score = [320,230,480,120]
 
-// if(num1 < num2){
-//     alert("this is larger")
-// }
-// if(num1 === num2){
-//     alert("this is Equal")
-// }
+// score.sort()
 
-// ans no 3
-// var num = prompt("enter value");
-// if(num > 0){
-//     alert("this is positive")
-// }else if(num < 0){
-//     alert("this is negative")
-// }else if(num == 0){
-//     alert("this is equal")
-// }
-
-// ans no 4
-// var vowels = prompt("enter vowels")
-
-// if(vowels == "a" || vowels =="A"){
-//     alert("Your answer is correct")
-// }else if(vowels == "e" || vowels =="E"){
-//     alert("Your answer is correct")
-// }else if(vowels == "i" || vowels =="I"){
-//     alert("Your answer is correct")
-// }else if(vowels == "o" || vowels =="O"){
-//     alert("Your answer is correct")
-// }else if(vowels == "u" || vowels =="U"){
-//     alert("Your answer is correct")
-// }else {
-//  alert("not correct")
-// }
-
-// ans no 5
-// var pass = "helloworld"
-
-// var val = prompt("enter password");
-
-// if(val == ""){
-// alert("Please enter your password")
-// }else if(pass == val){
-//     alert("Correct! The password you entered matches the original password.")
-// }else{
-//     alert("incorrect")
-// }
-
-// ans no 6
-// var greeting;
-// var hour = 13;
-// if (hour > 18) {
-// greeting = "Good day";
-// console.log(greeting)
-// }
-// else{
-// greeting = "Good evening";
-// console.log(greeting)
-// }
-
-// ans no 7
-var time = +prompt("enter time in 24 hours format")
-
-if(time > 12){
-    alert(cls)
-}else{
-    alert(time)
-}
+// console.log(score)
 
 
+// Answer no 11
+
+// var cities = ["karachi" , "lahore ", "Islamabad" ,"Quetta","Peshawar"]
+// var hell = cities.slice(2,4)
+// console.log(hell)
+
+// Answer no 12
+
+// var arr = ["This ", " is ", " my ", " cat"];
+
+// document.write(arr + "<br />")
+// document.write(arr.join(" "))
+
+
+// Answer no 13
+
+
+// Answer no 14
+
+// var mob = ["Apple","Samsung", "Motorola", "Nokia", "Sony" ,"Haier"];
+// document.write("<select><option>"+mob[0]+"</option><option>"+mob[1]+"</option><option>"+mob[2]+"</option><option>"+mob[3]+"</option><option>"+mob[4]+"</option><option>"+mob[5]+"</option></select>")
