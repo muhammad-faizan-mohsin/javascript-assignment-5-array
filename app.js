1732de9ef50dfcf6c5a4c7c4467241ba450c7b65
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-02-15 03:26:59 +0500

    section 2 half compeleted

--- app.js
+++ app.js
@@ -1,117 +1,22 @@
-// array
 // Answer no 1
-// var a = ["faizan","hasnain"];
+// var a = [[],[]];
 
 // Answer no 2
-// var a = new Array("faizan","hussain")
-// console.log(a)
 
-//Answer no 3 
-// var a = ["faizan", "hassan","kashan"];
-// console.log(a)
+// var a = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// var b = a[0].join(" ") +"<br>"+ a[1].join(" ")+"<br>"+a[2].join(" ")+"<br>"
+// document.write(b)
+
+// Answer no 3
+
+// for(i = 1; i <= 10; i++){
+// document.write(i + "<br/>")
+// }
 
 // Answer no 4
-// var a = [0, 1,2];
-// console.log(a)
+var num = prompt("Enter number");
+var table = prompt("Enter table length");
 
-// Answer no 5
-// var a = [true, false ,true];
-// console.log(a)
-
-// Answer no 6
-// var a = ["faizan", 4 ,true];
-// console.log(a)
-
-// Answer no 7
-// var edu = ["SSC","HSC","BCS","BS","BCOM","MS","M. Phil","PhD"]
-
-// document.write("<h1>Qualification</h1>"+
-//     "<ol><li>"+edu[1]+"</li> "+
-//     "<li>"+edu[2]+"</li>"+
-//     "<li>"+edu[3]+"</li>"+
-//     "<li>"+edu[4]+"</li>"+
-//     "<li>"+edu[5]+"</li>"+
-//     "<li>"+edu[6]+"</li>"+
-//     "<li>"+edu[7]+"</li>"+
-//     "<li>"+edu[8]+"</li>"+
-//     "<li>"+edu[9]+"</li> </ol>"
-
-// )
-
-// Answer no 8
-// var stu = ['faizan','hussain','hasnain'];
-// var marks = [320,230,480]
-// var tmarks = 500;
-// var per = marks[0] / tmarks * 100;
-// var per2 = marks[1] / tmarks * 100;
-// var per3 = marks[2] / tmarks * 100;
-
-// document.write("score of "+ stu[0] +" is "+marks[0] +" precentage "+ per +"%"+"<br/>")
-// document.write("score of "+ stu[1] +" is "+marks[1] +" precentage "+ per2+"%"+"<br/>")
-// document.write("score of "+ stu[2] +" is "+marks[2] +" precentage "+ per3+"%"+"<br/>")
-
-
-
-// Answer no 9
-// var clr = ["red", "blue", "green"]
-
-
-// clr.unshift(prompt("enter"))
-// clr.push(prompt("enter"))
-// console.log(clr)
-
-// clr.push("purple")
-
-// console.log(clr)
-
-// clr.unshift("orange","pink")
-
-// console.log(clr)
-
-// clr.shift("orange","pink")
-
-// console.log(clr)
-
-// clr.pop("orange","pink")
-
-// console.log(clr)
-
-// clr.splice(2,1,"lemon")
-
-// console.log(clr)
-
-
-// clr.splice(2,1)
-
-// console.log(clr)
-
-// Answer no 10
-
-// var score = [320,230,480,120]
-
-// score.sort()
-
-// console.log(score)
-
-
-// Answer no 11
-
-// var cities = ["karachi" , "lahore ", "Islamabad" ,"Quetta","Peshawar"]
-// var hell = cities.slice(2,4)
-// console.log(hell)
-
-// Answer no 12
-
-// var arr = ["This ", " is ", " my ", " cat"];
-
-// document.write(arr + "<br />")
-// document.write(arr.join(" "))
-
-
-// Answer no 13
-
-
-// Answer no 14
-
-// var mob = ["Apple","Samsung", "Motorola", "Nokia", "Sony" ,"Haier"];
-// document.write("<select><option>"+mob[0]+"</option><option>"+mob[1]+"</option><option>"+mob[2]+"</option><option>"+mob[3]+"</option><option>"+mob[4]+"</option><option>"+mob[5]+"</option></select>")
+for(i = 0 ; i < table.length ; i++){
+    document.write(nums +" X "+ table[i]+" = "+num*table +"<br/>"  )
+} 
