4f7548a1123242785669d56095e8df07b9059a55
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-02-23 02:32:38 +0500

    2 question left

--- app.js
+++ app.js
@@ -14,9 +14,91 @@
 // }
 
 // Answer no 4
-var num = prompt("Enter number");
-var table = prompt("Enter table length");
+// var num = prompt("Enter number");
+// var table = prompt("Enter table length");
 
-for(i = 0 ; i < table.length ; i++){
-    document.write(nums +" X "+ table[i]+" = "+num*table +"<br/>"  )
-} 
+
+// for(var i = 1 ; i <= table ; i++){
+//     document.write(num +" X "+ i +" = "+ num*i +"<br/>" )
+// }
+
+
+// Answer no 5
+
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"];
+
+// for(var i = 0 ; i < fruits.length; i++){
+//     document.write("Element at index is "+i+ " is " + fruits[i]+"<br/>")
+// }
+
+
+// Answer no 6
+
+// a
+// for(var i = 1; i <= 15 ; i++){
+//     console.log(i)
+// }
+
+// b
+
+// for(var i = 10; i > 0 ; i--){
+//     console.log(i)
+// }
+
+// c
+// for(var i = 0; i <= 10; i++){
+//     console.log(i*2)
+// }
+
+// d
+
+// for(var i = 1; i <= 20; i=i+2){
+//         document.write(i + "<br/>")
+//     } 
+
+
+    // e
+// for(var i = 1; i <= 10; i++){
+//     console.log(i*2+"k")
+// }
+
+
+
+
+
+
+
+
+  // answer no 7
+//    var a  = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var b = prompt("Enter ")
+// for(var i = 0; i < a.length; i++){
+    
+// if(a[i] == b){  
+//     console.log(b+" is available at index "+ i +" in Our bakery " )
+//     break   
+// }
+
+
+// }
+// if(a[i]!== b){
+//     console.log(b+" not available at index in Our bakery " )
+
+// }
+
+
+    // answer no 8
+
+
+    // var a = [24, 53, 78, 91, 12]
+
+
+
+
+// Answer no 10
+// for(var i = 5; i <= 100; i=i+5){
+//     console.log(i)
+// }
+
+
+
